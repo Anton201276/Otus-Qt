601863cdb65534d8d21e0556c8d241de43b027dd
Антропов Антон Александрович <aaa@nposibmach.ru>
2026-03-26 17:55:52 +0700

    подключение сигналов из QML в C++

---
+++ GraduateWork/src/controlsimulator.cpp
@@ -0,0 +1,6 @@
+#include "controlsimulator.h"
+
+ControlSimulator::ControlSimulator(QObject *parent) : QObject(parent)
+{
+
+}
--- GraduateWork/src/main.cpp
+++ GraduateWork/src/main.cpp
@@ -1,14 +1,46 @@
 #include <auroraapp.h>
 #include <QtQuick>
 
+#include "controlsimulator.h"
+
 int main(int argc, char *argv[])
 {
     QScopedPointer<QGuiApplication> application(Aurora::Application::application(argc, argv));
     application->setOrganizationName(QStringLiteral("ru.template"));
     application->setApplicationName(QStringLiteral("DiplomaApp"));
 
+    ControlSimulator controlSim;
+
+
     QScopedPointer<QQuickView> view(Aurora::Application::createView());
+
+
+    // ВАЖНО: устанавливаем свойство контекста ДО загрузки QML
+    //    view->engine()->rootContext()->setContextProperty("controlSim", &controlSim);
+
+    // Регистрируем тип для использования в QML
+    //qmlRegisterType<ControlSimulator>("ru.template.DiplomaApp", 1, 0, "MyHandler");
+
     view->setSource(Aurora::Application::pathTo(QStringLiteral("qml/DiplomaApp.qml")));
+
+
+    if (!view->rootObject()) {
+            qCritical() << "Не удалось загрузить QML-файл";
+            return -1;
+    }
+
+    // Получаем корневой объект QML
+    QObject *rootObject = view->rootObject();
+
+        // Подключаем сигнал QML к слоту C++
+        QObject::connect(rootObject, SIGNAL(dataSent(QString,int)),
+                        &controlSim, SLOT(onDataReceived(QString,int)));
+
+        QObject::connect(rootObject, SIGNAL(messageSent(QString)),
+                        &controlSim, SLOT(onMessageReceived(QString)));
+
+
+
     view->show();
 
     return application->exec();
